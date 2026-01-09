9cde733a6b07b4074783aac6504de7a64e44dbad
Stepan <bai_777@mail.ru>
2026-01-09 18:38:47 +0300

    добавил интерфейс Eatable.java, реализовал класс Animal.java

--- src/entity/Animal.java
+++ src/entity/Animal.java
@@ -1,4 +1,36 @@
 package entity;
 
-public class Animal {
+public abstract class Animal implements Eatable {
+    private double weight;
+    private int maxCountInCell;
+    private int speed;
+    private double foodNeededForSaturation;
+
+    public Animal(double weight, int maxCountInCell, int speed, double foodNeededForSaturation) {
+        this.weight = weight;
+        this.maxCountInCell = maxCountInCell;
+        this.speed = speed;
+        this.foodNeededForSaturation = foodNeededForSaturation;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public int getMaxCountInCell() {
+        return maxCountInCell;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public double getFoodNeededForSaturation() {
+        return foodNeededForSaturation;
+    }
+
+    public abstract void eat(Eatable food);
+    public abstract void move();
+    public abstract void multiply();
+    public abstract void die();
 }
--- src/entity/Eatable.java
+++ src/entity/Eatable.java
@@ -1,4 +1,4 @@
 package entity;
 
-public class Eatable {
+public interface Eatable {
 }
