a19af8c04af1a680cecb07a1f91266c0f5d44c9a
Stepan <bai_777@mail.ru>
2026-01-11 20:02:03 +0300

    переделал класс Location

--- src/logic/Location.java
+++ src/logic/Location.java
@@ -3,16 +3,17 @@ package logic;
 import config.Settings;
 import entity.Animal;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Location {
     private Map<Class<? extends Animal>, Integer> animals;
     private int plantsCount;
+    private List<Animal> animalObjects;
 
     public Location() {
         this.animals = new HashMap<>();
         this.plantsCount = 0;
+        this.animalObjects = new ArrayList<>();
     }
 
     public boolean addAnimal(Animal animal) {
@@ -31,9 +32,42 @@ public class Location {
         int count = animals.getOrDefault(animalClass, 0);
         if (count > 0) {
             animals.put(animalClass, count - 1);
+            for (Iterator<Animal> iterator = animalObjects.iterator(); iterator.hasNext(); ) {
+                Animal animal = iterator.next();
+                if (animal.getClass().equals(animalClass)) {
+                    iterator.remove();
+                    break;
+                }
+            }
             return true;
         }
         return false;
+    }
+
+    public boolean removeAnimal(Animal animal) {
+        Class<? extends Animal> animalClass = animal.getClass();
+        int count = animals.getOrDefault(animalClass, 0);
+        if (count > 0 && animalObjects.remove(animal)) {
+            animals.put(animalClass, count - 1);
+            return true;
+        }
+        return false;
+    }
+
+    public List<Animal> getAnimalObjects() {
+        return new ArrayList<>(animalObjects);
+    }
+
+    public void cleanDeadAnimals() {
+        Iterator<Animal> iterator = animalObjects.iterator();
+        while (iterator.hasNext()) {
+            Animal animal = iterator.next();
+            if (!animal.isAlive()) {
+                iterator.remove();
+                Class<? extends Animal> animalClass = animal.getClass();
+                animals.put(animalClass, animals.get(animalClass) - 1);
+            }
+        }
     }
 
     public int getAnimalCount(Class<? extends Animal> animalClass) {
@@ -66,10 +100,4 @@ public class Location {
     public void setPlantsCount(int plantsCount) {
         this.plantsCount = Math.min(plantsCount, Settings.MAX_PLANTS_IN_CELL);
     }
-
-    public boolean hasSpaceFor(Class<? extends Animal> animalClass, Animal animal) {
-        int currentCount = animals.getOrDefault(animalClass, 0);
-        return currentCount < animal.getMaxCountInCell();
-    }
-
 }
